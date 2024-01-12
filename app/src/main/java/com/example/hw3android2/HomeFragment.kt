dbe6ef237d8b6a707bab0d295d597f12144ee8b3
pubgkonfuz <mashanloskm@gmail.com>
2024-01-12 16:08:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw3android2/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.hw3android2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hw3android2.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding:FragmentHomeBinding? = null
+    private val binding:FragmentHomeBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hw3android2/SecondFragment.kt
@@ -0,0 +1,43 @@
+package com.example.hw3android2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.hw3android2.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding: FragmentSecondBinding get() = _binding!!
+    private val preferencesHelper: PreferencesHelper by lazy {
+        PreferencesHelper(context ?: requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onStartClick()
+    }
+
+    private fun onStartClick() {
+        binding.btnScip.setOnClickListener {
+            preferencesHelper.isShownOnBoard = true
+            findNavController().navigate(R.id.action_boardFragment_to_homeFragment)
+        }
+    }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+    }
